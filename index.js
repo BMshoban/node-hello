ef340adede18480bef9a6e7a8c270d7353165bde
devopss projects practice <shoban200327@gmail.com>
2025-10-30 12:03:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'DEPLOYED by SHOBAN B DEVOPS Intern !\n'
+  const msg = 'DEPLOYED by SHOBAN DEVOPS Intern !\n'
   res.end(msg);
 });
 
