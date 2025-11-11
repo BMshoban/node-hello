864ca89efcab43b462a585e5270bf88d5910be16
devopss projects practice <shoban200327@gmail.com>
2025-11-11 19:13:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'DEPLOYED by SHOBAN using git DEVOPS at mxfy...  :)  !\n'
+  const msg = '$DEPLOYED by SHOBAN using git DEVOPS at mxfy...  :)$ !\n'
   res.end(msg); 
 });
 
